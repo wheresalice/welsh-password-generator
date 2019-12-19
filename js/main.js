b887f67102a405f5d875a135879b8f81a88c8cff
Alice <WheresAlice@users.noreply.github.com>
2019-12-19 12:37:19 +0000

    make password more secure

--- js/main.js
+++ js/main.js
@@ -1,9 +1,29 @@
-$.getJSON("/wordlist.json", function(wordlist) {
-  password = [];
-  for (var i = 0; i < 3; i++) {
-    word = wordlist['words'][Math.floor(Math.random() * wordlist['words'].length)];
-    password[i] = word.charAt(0).toUpperCase() + word.slice(1);
+const CHARACTERS = "0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+
+(function() {
+  let rng = window.crypto || window.msCrypto;
+  if (rng === undefined)
+    return;
+
+  console.log("upgrading to secure rng");
+  Math.random = function() {
+    return rng.getRandomValues(new Uint32Array(1))[0] / 4294967296;
+  };
+})();
+
+function maybePush(myArray) {
+  let char = CHARACTERS[Math.floor(Math.random() * CHARACTERS.length)];
+  if (Math.random() > 0.5) {
+    return myArray.push(char)
   }
-  password.push(Math.floor(Math.random() * 10));
+}
+
+$.getJSON("/wordlist.json", function(wordlist) {
+  let password = [];
+  for (var i = 0; i < 3; i++) {
+    let word = wordlist['words'][Math.floor(Math.random() * wordlist['words'].length)];
+    password.push(word.charAt(0).toUpperCase() + word.slice(1));
+    maybePush(password);
+  }
   $('#password').html(password.join(''))
 });
